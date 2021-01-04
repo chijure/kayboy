e73f9d1046c9762af3743a529f5d3c8daa3c0987
Jhuan Reategui Solis <reateguisolisjm@gmail.com>
2021-01-04 00:28:16 -0500

    KaiBoy name in "pack-to-omni.sh"

--- pack-to-omni.sh
+++ pack-to-omni.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-TMPDIR="kava-omni"
-OUTFILE="kava-omni.zip"
+TMPDIR="KaiBoy-omni"
+OUTFILE="KaiBoy-omni.zip"
 rm -f $OUTFILE
 mkdir -p $TMPDIR
 touch ${TMPDIR}/update.webapp
 zip -9 -r --exclude=*${TMPDIR}* --exclude=*.git* --exclude=*.DS_Store* --exclude=README.md --exclude=pack-to-omni.sh ${TMPDIR}/application.zip .
 ORIGIN="$(grep origin manifest.webapp | cut -d '"' -f 4)"
-echo {\"version\": 1, \"manifestURL\": \"${ORIGIN}/manifest.webapp\"} > ${TMPDIR}/metadata.json
+echo {\"version\": 1, \"manifestURL\": \"${ORIGIN}/manifest.webapp\"} >${TMPDIR}/metadata.json
 cd $TMPDIR
 zip -9 -r ../${OUTFILE} .
 cd ..
